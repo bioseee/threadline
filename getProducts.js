0d6707c1f43f012758840ee1ba4c561dad2fbd3a
Biose <favbiose@gmail.com>
2026-08-15 02:48:45 -0700

    fetching products from external api

---
+++ getProducts.js
@@ -0,0 +1,14 @@
+async function getProducts(){
+    try{
+        const response = await fetch("https://fakestoreapi.com/products")
+        if(!response.ok){
+            throw new Error("error fetching products")
+        }
+        const products = await response.json()
+        return products
+    }catch(err){
+        console.error(err)
+        throw err
+    }
+    
+}
